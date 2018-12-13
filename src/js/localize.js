ed89f5d87be9c5fc1f90c4f2280ca6cc6a3bf2f0
Radek F <radek.fr@email.cz>
2018-12-13 23:40:13 +0100

    adding projects description styles

--- src/js/localize.js
+++ src/js/localize.js
@@ -9,7 +9,10 @@ i18next.init({
                 "about-heading": "Ahoj, jmenuji se Radovan",
                 "about-text": `Jsem začínající front-end developer z Kroměříže.
                         Od doby ukončení studia jsem vystřídal různá zaměstnání, která mě ale nikdy moc nebavila.
-                        Asi před rokem jsem se začal ve svém volném čase zajímat o web development což se mě drží dodnes`
+                        Asi před rokem jsem se začal ve svém volném čase zajímat o web development a od té doby se 
+                        neustále zlepšuji a poznávám nové technologie`,
+                "projects-heading": "Mé projekty",
+                "projects-subheading": "Tady jsou některé z mých projektů"        
             }
         },
         en:{
@@ -17,7 +20,9 @@ i18next.init({
                 "about-heading": "Hello, my name is Radovan",
                 "about-text": `I am a begginer fron-end developer from Czech Republic.
                               Since the time i finished my studies i have had few different jobs, none of which particulary interested me.
-                              About a year ago i tried web-development and i have been stuck with it since then.`
+                              About a year ago i tried web-development and then i am constantly improving myself and learning new technologies.`,
+                "projects-heading": "My Projects",
+                "projects-subheading": "Here are some of my projects"                                    
             }
         }
     }
@@ -28,6 +33,8 @@ i18next.init({
 function updateContent (){
     document.querySelector(".about-heading").innerHTML = i18next.t("about-heading")
     document.querySelector(".about-text").innerHTML = i18next.t("about-text")
+    document.querySelector(".projects-heading").innerHTML = i18next.t("projects-heading")
+    document.querySelector(".projects-subheading").innerHTML = i18next.t("projects-subheading")
 }
 
 const changeLang = (lang) => {
