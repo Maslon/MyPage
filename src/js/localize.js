bc7c657cec03f3a581ee0ddc510c8181889fea94
maslo <radek.fr@email.cz>
2019-02-25 19:45:15 +0100

    text changes

--- src/js/localize.js
+++ src/js/localize.js
@@ -8,9 +8,10 @@ i18next.init({
             translation: {
                 "about-heading": "Ahoj, jmenuji se Radovan",
                 "about-text": `Jsem začínající front-end developer z Kroměříže.
-                        Od doby ukončení studia jsem vystřídal různá zaměstnání, která mě ale nikdy moc nebavila.
+                        Od doby ukončení studia jsem vystřídal různá zaměstnání.
                         Asi před rokem jsem se začal ve svém volném čase zajímat o web development a od té doby se 
-                        neustále zlepšuji a poznávám nové technologie`,
+                        neustále zlepšuji, poznávám nové technologie a snažím se vypilovat své stávající znalosti.
+                        Čím víc toho vím o web-developmentu tím je pro mě psaní kódu a řešení problémů větší zábava.`,
                 "projects-heading": "Mé projekty",
                 "projects-subheading": "Tady jsou některé z mých projektů",
                 "contact-text": "Kontakt"       
@@ -20,8 +21,9 @@ i18next.init({
             translation: {
                 "about-heading": "Hello, my name is Radovan",
                 "about-text": `I am a begginer front-end developer from Czech Republic.
-                              Since the time i finished my studies i have had few different jobs, none of which particulary interested me.
-                              About a year ago i tried web-development and since then i am constantly improving myself and learning new technologies.`,
+                              Since the time i finished my studies i have had few different jobs.
+                              About a year ago i tried web-development and since then i am constantly improving myself and learning new technologies.
+                              The more i know about coding the the more exciting and fun it is to solve various problems with it.`,
                 "projects-heading": "My Projects",
                 "projects-subheading": "Here are some of my projects",
                 "contact-text": "Contact"                                     
